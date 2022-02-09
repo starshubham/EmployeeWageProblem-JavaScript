5381a5ce46453fa021f2aec8bcff2e58f56b4e07
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-09 16:06:04 +0530

    [add] Shubham added UC6 of EmpWageProblemJS

--- EmpWage.js
+++ EmpWage.js
@@ -87,3 +87,29 @@ function getWorkingHours(empCheck) {
     console.log("UC5 - Total Days: " + totalWorkingDays +
         ", Total Hrs: " + totalEmpHrs + ", Emp Wage: " + empWage);
 }
+
+
+/* UC 6: Store the Daily Wage along with the Total Wage
+        - Save in an Array the Daily Wage */
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+    totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: " + totalWorkingDays + 
+            " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
