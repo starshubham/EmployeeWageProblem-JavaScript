2688f3317d2304e320df43513cd857eb7ac80a24
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-09 12:27:16 +0530

    [add] Shubham added UC3 of EmpWageProblemJS

--- EmpWage.js
+++ EmpWage.js
@@ -36,3 +36,22 @@ const WAGE_PER_HOUR = 20;
     console.log("UC2 - Emp Wage: " + empWage);
 }
 
+
+// UC 3: Refactored code to write function for daily working Hours
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+{
+    let empHrs = 0;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("UC3 - Emp Wage: " + empWage, " Total Working Hours: " + empHrs);
+}
