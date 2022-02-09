4a1dc8431f5b25bfbfc4a18a0266ab994840d9c3
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-09 14:39:43 +0530

    [add] Shubham added UC5 of EmpWageProblemJS

--- EmpWage.js
+++ EmpWage.js
@@ -60,11 +60,30 @@ function getWorkingHours(empCheck) {
 // UC 4: Calculating Wages for a Month assuming 20 Working Days in a Month
 {
     const NUM_OF_WORKING_DAYS = 20;
-    let totalEmpHrs = 0;
+    let empHrs = 0;
     for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        empHrs += getWorkingHours(empCheck);
+    }
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("UC 4:- Total Hrs: " + empHrs + " Emp Wage: " + empWage);
+}
+
+
+/* UC 5: Calculate Wages till a condition of total working hours of 160 or
+         max days of 20 is reached for a month */
+{
+    const MAX_HRS_IN_MONTH = 160;
+    const NUM_OF_WORKING_DAYS = 20;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
         let empCheck = Math.floor(Math.random() * 10) % 3;
         totalEmpHrs += getWorkingHours(empCheck);
     }
     let empWage = totalEmpHrs * WAGE_PER_HOUR;
-    console.log("UC 4:- Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+    console.log("UC5 - Total Days: " + totalWorkingDays +
+        ", Total Hrs: " + totalEmpHrs + ", Emp Wage: " + empWage);
 }
