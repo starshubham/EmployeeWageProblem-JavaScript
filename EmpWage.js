d448b864da1d27f10d4bd1b74a87a01bd0e7c350
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-09 11:25:12 +0530

    [add] Shubham added EmpWageProblemJS

---
+++ EmpWage.js
@@ -0,0 +1,12 @@
+// UC1:- Ability to Check Employee is present or Absent
+{
+    const IS_ABSENT = 0;
+
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if (empCheck == IS_ABSENT) {
+        console.log("UC1 - Employee is ABSENT");
+    }
+    else {
+        console.log("UC1 - Employee is PRESENT");
+    }
+}
