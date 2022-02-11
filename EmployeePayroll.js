57ef62cec49e76b86df0623b587276a69f3fc35b
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-11 12:20:58 +0530

    [add] Shubham added UC15 of EmployeePayrollData Problem

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -24,8 +24,8 @@
         set id(id){ this._id = id; }
         get name() { return this._name; }
         set name(name) { this._name = name; }
-        get salary() { return this._gender; }
-        set salary(salary) { this._gender = gender; }
+        get salary() { return this._salary; }
+        set salary(salary) { this._salary = salary; }
         
         //methid to return everything in string
         toString() {
@@ -85,8 +85,8 @@ console.log("\n*** UC14 ***");
             else throw 'Name is Incorrect!';
         }
 
-        get salary() { return this._gender; }
-        set salary(salary) { this._gender = gender; }
+        get salary() { return this._salary; }
+        set salary(salary) { this._salary = salary; }
         
         //methid to return everything in string
         toString() {
@@ -113,4 +113,106 @@ console.log("\n*** UC14 ***");
 
     let newEmployeePayroll = new EmployeePayroll(1, "Ram", 40000, "M", new Date());
     console.log(newEmployeePayroll.toString());    
+}
+
+
+/* UC 15: Ability to check the employee id and salary are non zero positive
+          number, the gender is M or F and date is not a future date
+          - Use Regex Pattern
+          - Use Try Catch in case of Error
+*/
+{
+    console.log("\n*** UC15 ***");
+    class EmployeePayroll
+    {
+        //properties of class
+        id;
+        salary;
+        gender;
+        startDate;
+    
+        //defining constructor of class using constructor keeyword
+        constructor(...params)
+        {
+            this.id = params[0];
+            this.name = params[1];
+            this.salary = params[2];
+            this.gender = params[3];
+            this.startDate = params[4];
+        }
+    
+        // getter and setter methods
+        get id() { return this._id };
+        set id(id) {
+            //pattern for id for positive numbers
+            let idRegex = RegExp('^[1-9]{1}[0-9]$');
+            if (idRegex.test(id))
+                this._id = id;
+            else throw "Error! Id format is Incorrect";
+        }
+
+        get name() { return this._name; }
+        set name(name) { 
+            // pattern for name. Name starts with capital and has atleast 3 characters 
+            let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+            if (nameRegex.test(name))
+                this._name = name;
+            else throw 'Error! Name is Incorrect!';
+        }
+
+        get salary() { return this._salary; }
+        set salary(salary) {
+            // pattern for salary for non zero positive numbers
+            let salaryPattern = RegExp('^[1-9][0-9]*$');
+            if (salaryPattern.test(salary))
+                this._salary = salary;
+            else
+                throw "Error! Salary format is Incorrect";
+        }
+        
+        get gender() { return this._gender; }
+        set gender(gender) {
+            // pattern for gender either M or F
+            let genderPattern = RegExp('M|F');
+            if (genderPattern.test(gender))
+                this._gender = gender;
+            else
+                throw "Error! Gender format is Incorrect";
+        }
+
+        get startDate() { return this._startDate };
+        set startDate(date) {
+            // Given date should not exceed todays date
+            let givenDate = Date(date); //mm-dd-YYYY
+            let today = new Date();
+            if (givenDate <= today)
+                this._startDate = date;
+            else
+                throw "Error! Date is greater than current date";
+        }
+        //methid to return everything in string
+        toString() {
+            return "Id = " + this.id + ", Name = " + this.name + ", Salary = " + this.salary + ", " +
+            "Gender = " + this.gender + ", StartDate = " + this.startDate;
+        }
+    }
+    
+    // Object for class using parameterized conbstructor
+    let empPayroll = new EmployeePayroll(1, "Mark", 30000);
+    console.log(empPayroll.toString());
+    //setting values to the properties of class using try catch
+    try {
+        empPayroll.id = 2;
+        empPayroll.name = "Shubham";
+        empPayroll.salary = 50000;
+        empPayroll.gender = 'M';
+        empPayroll.startDate = '07-12-2021';//MM-DD-YYYY
+        console.log(empPayroll.toString());
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+    let newEmployeePayroll = new EmployeePayroll(1, "Ram", 40000, "M", new Date());
+    console.log(newEmployeePayroll.toString());        
 }
